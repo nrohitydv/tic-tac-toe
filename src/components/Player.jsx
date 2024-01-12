47e8f4d0ee4acb163458c92ea55c2a7c368fed82
Rohit <rohityadav787898@gmail.com>
2024-01-12 22:19:48 +0545

    build name board and game board

---
+++ src/components/Player.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  //let btnCaption = "Edit";
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+    //btnCaption = "Save";
+  }
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
+
+export default Player;
